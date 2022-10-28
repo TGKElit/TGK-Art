8d5d81cd528bfb59351f820597281cb4cdfe4e83
TGKElit <tgkelit@gmail.com>
2022-10-28 17:06:55 +0200

    Fixed link to home.php

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     </head>
     <body>
     <nav>
-        <a href="./home" class="clickable"><h2>Home</h2></a>
+        <a href="./home.php" class="clickable"><h2>Home</h2></a>
         <a href="./gallery.php" class="clickable"><h2>Gallery</h2></a>
         <a href="./animation.php" class="clickable"><h2>Animation</h2></a>
         <a href="./store.php" class="clickable"><h2>Store</h2></a>
