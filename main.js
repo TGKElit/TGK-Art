68becd30c52bdc3560ee4a23f4e785793e2c5b9e
TGKElit <tgkelit@gmail.com>
2022-11-07 23:39:27 +0100

    filterMenu now stays open on refresh

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+let openess = sessionStorage.getItem('openess');
+if (!openess){
+  openess = "none";
+}
+filterMenu.style.display = openess;
+
 function selectImage() {
     const selections = document.querySelectorAll('.gallerySection');
     for (const selected of selections) {
@@ -9,8 +15,11 @@ function openMenu() {
     const filterMenu = document.getElementById('filterMenu');
     if(filterMenu.style.display == "block") {
       filterMenu.style.display = "none";
+      sessionStorage.setItem('openess', 'none');
     }
     else {
       filterMenu.style.display = "block";
+      sessionStorage.setItem('openess', 'block');
     }
 }
+
